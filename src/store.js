ac02b5303ebfcd94d9d773484a0dbb475a956136
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-01-07 20:52:23 +0100

    chore: add action creators

--- src/store.js
+++ src/store.js
@@ -38,14 +38,43 @@ function reducer(state = initialState, action) {
 }
 
 const store = createStore(reducer);
-store.dispatch({ type: "account/deposit", payload: 500 });
+// store.dispatch({ type: "account/deposit", payload: 500 });
+// // console.log(store.getState());
+
+// store.dispatch({
+//   type: "account/requestLoan",
+//   payload: { amount: 1000, purpose: "Buy a car" },
+// });
 // console.log(store.getState());
 
-store.dispatch({
-  type: "account/requestLoan",
-  payload: { amount: 1000, purpose: "Buy a car" },
-});
-console.log(store.getState());
+// store.dispatch({ type: "account/payLoan" });
+// console.log(store.getState());
 
-store.dispatch({ type: "account/payLoan" });
+// ACTION CREATORS
+function deposit(amount) {
+  return { type: "account/deposit", payload: amount };
+}
+
+function withdraw(amount) {
+  return { type: "account/withdraw", payload: amount };
+}
+
+function requestLoan(amount, purpose) {
+  return {
+    type: "account/requestLoan",
+    payload: { amount: amount, purpose },
+  };
+}
+
+function payLoan() {
+  return {
+    type: "account/payLoan",
+  };
+}
+
+// MOCK
+store.dispatch(deposit(500));
+store.dispatch(withdraw(400));
+store.dispatch(requestLoan(200, "Buy house"));
+store.dispatch(payLoan());
 console.log(store.getState());
