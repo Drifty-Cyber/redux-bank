ea31b1a5e3cc98dff5f9469954fee4f82a7c54ea
Drifty-Cyber <raphaelfadimu@gmail.com>
2024-01-12 03:33:37 +0100

    chore: refactor customerSlice

--- src/features/customers/customerSlice.js
+++ src/features/customers/customerSlice.js
@@ -1,9 +1,42 @@
-const initialStateCustomer = {
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
   fullName: "",
   nationalID: "",
   createdAt: "",
 };
 
+const customerSlice = createSlice({
+  name: "customer",
+  initialState,
+  reducers: {
+    createCustomer: {
+      prepare(fullName, nationalID) {
+        return {
+          payload: {
+            fullName,
+            nationalID,
+            createdAt: new Date().toISOString(),
+          },
+        };
+      },
+      reducer(state, action) {
+        state.fullName = action.payload.fullName;
+        state.nationalID = action.payload.nationalID;
+        state.createdAt = new Date().toISOString();
+      },
+    },
+    updateName(state, action) {
+      state.fullName = action.payload;
+    },
+  },
+});
+
+export const { createCustomer, updateName } = customerSlice.actions;
+
+export default customerSlice.reducer;
+
+/*
 export default function customerReducer(state = initialStateCustomer, action) {
   switch (action.type) {
     case "customer/createCustomer":
@@ -40,3 +73,4 @@ export function updateName(fullName) {
     payload: fullName,
   };
 }
+*/
